fd4d5def4a9b9fa42795bf6d89a7b3e26a587713
Ezequielo <ezeoyhambure@gmail.com>
2024-07-28 10:03:13 -0300

    Finished Deck, Started Bank, Updated README.md
    ## Deck
    - Moved into its own folder
    - added shuffle() and deal() to Deck struct
    - added the rand package
    - attempted to make use of Result for something akin to error handling
    
    ## Bank
    - created new cargo
    
    ## Readme
    - Added basic explanation of the purpose of this repo
    
    ## .gitignore
    - added language and IDE dependent files to it

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# rust-practice
+# Personal Rust Practice
+## # IMPORTANT # ##
+This is NOT a repo for you to practice rust, this is a repo for me to
+upload my smaller projects made while following the course
+[Rust: The Complete Developer's Guide](https://www.udemy.com/course/rust-the-complete-developers-guide)
+by Stephen Grider.
+
+You can read it, check it, and post issues to correct things I do wrong.
+
+This is not really a guideline or an order, but more of a warning so that you don't waste
+time trying to practice with this... _"code"_
--- deck/src/main.rs
+++ deck/src/main.rs
@@ -1,4 +1,6 @@
-#[derive(Debug)] //Defines attributes for the Deck struct, it gives the compiler extra instructions
+use rand::{thread_rng, seq::SliceRandom};
+
+#[derive(Debug)] //Defines attributes for the deck struct, it gives the compiler extra instructions
 // `derive` is an attribute that specifies traits to automatically apply to the struct
 // debug is a trait, traits are a set of functions
 struct Deck{
@@ -25,7 +27,7 @@ impl Deck {
             "Ten",
             "Jack",
             "Queen",
-            "King"
+            "King",
         ];
         // "variables" or bindings in rust are immutable by default
         // to have one mutable, you need to add 'mut' when declaring it
@@ -37,24 +39,49 @@ impl Deck {
             }
         }
         // Rust returns the last line's value if no return is specified
-        // this is the same as running return Deck {cards};
+        // this is the same as running return deck {cards};
         // This doesn't work if there is a ';' since that would make it a fn
         Deck { cards }
         // if I run 1 == 0 as a last line the function would return the result of that
         //1 == 0
     }
-    fn shuffle(&self){
-        // TODO: Add shuffling mechanism
+    fn shuffle(&mut self){
+        let mut randomizer = thread_rng();
+        self.cards.shuffle(&mut randomizer);
+    }
+    fn deal(&mut self, amount: usize) -> Result<Vec<String>, &str>{
+        let check: Result<Vec<String>, &str>;
+        if amount > self.cards.len(){
+            check = Err("You asking for too much, chief");
+            println!("{:?}", check);
+            return check;
+        }
+        check = Ok(self.cards.split_off( self.cards.len() - amount));
+        println!("{:?}", check);
+        return check;
     }
 }
 
 
 fn main() {
-    let deck = Deck::new();
+    let mut deck = Deck::new();
     deck.shuffle();
     // {} Tells the formatter to expect a value
     // {:?} Tells the formatter that it should "Prettify it" or prepare it to be human-readable
     // {:#?} Makes it easier to read the values as it formats it line by line
     println!("Here is your deck: {:#?}", deck.cards);
+    let hand = deck.deal(200);
+    if hand.is_ok(){
+        println!("Here is your hand: {:#?}", deck.deal(200));
+    }
+    else {
+        println!("Error: {:?}", hand.unwrap_err())
+    }
+    /*
+        Here is your hand: [
+            "Ace of Hearts",
+            "Two of Hearts",
+        ]
+     */
     println!("Hello, world!");
 }
