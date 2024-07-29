cb06b3ecf6dc48303ec4a11fd7883a8640c5e875
Ezequielo <ezeoyhambure@gmail.com>
2024-07-29 10:33:30 -0300

    Working on iterators
    Taking a break because udemy broke down, just like me fr fr /ironic

---
+++ iterators/src/functions/functions.rs
@@ -0,0 +1,35 @@
+pub fn print_elements(elements: &[String]) { // &[String] is a slice of Stings
+    // it is better to use this compared to Vec<String> because of memory and
+    // because slices are compatible with vec, but not vice versa.
+
+    // for element in elements {
+    //     println!("{}", element.to_string());
+    // }
+    elements
+        .iter()
+        //.map(|x| { format!("{}{}", x, x) })
+        .for_each(|element| println!("{}", element.to_string()));
+}
+pub fn shorten_strings(elements: &mut [String]) {
+    elements
+        .iter_mut() // Instead of just giving a borrowed value
+        // it gives a mutable borrowed value, allowing mutations
+        .for_each(|el| el.truncate(1));
+}
+pub fn to_uppercase(elements: &[String]) -> Vec<String> {
+    elements.iter()
+        .map(|x| { x.to_uppercase() })
+        //    .collect() // Collect implies the type of value based on the return type of the fn
+        .collect::<Vec<String>>() // We use this if there is no return type, or we want to save mem
+    //    .collect::<Vec<_>>() // also works and infers the value based on the iter
+}
+pub fn move_elements(from: Vec<String>, to: &mut Vec<String>) {
+    from.into_iter().for_each(|el| to.push(el.to_string()));
+}
+
+pub fn explode(elements: &[String]) -> Vec<Vec<String>> {
+    elements
+        .iter()
+        .map(|el| el.chars().map(|c| c.to_string()).collect())
+        .collect()
+}
---
+++ iterators/src/main.rs
@@ -0,0 +1,29 @@
+mod functions;
+use functions::functions::*;
+
+fn main() {
+    let mut colors = vec![
+        String::from("red"),
+        String::from("green"),
+        String::from("blue"),
+    ];
+    //print_elements(&colors);
+    //let screams = to_uppercase(&colors);
+    //print_elements(&screams);
+    //shorten_strings(&mut colors);
+    //print_elements(&colors);
+    //move_elements(screams, &mut colors);
+    print_elements(&colors);
+    println!("{:#?}", explode(&colors))
+    // let mut colors_iter = colors.iter(); //iter are completely new structs in memory
+    // They possess the following data
+    // 1. A pointer to the object they are iterating over
+    // 2. a pointer to the value they are currently accessing
+    // 3. a pointer to the end of the object (to catch if it went pass-the-element)
+    // colors_iter.next();
+    // Next() provides a Some(T) of the value that is present in the 2nd binding of the pointer
+    // it also moves that 2nd binding to the next value in the object being iterated
+    //
+    // Once it reaches the end of the object, the pointer to current value will be
+    // equal to pointer to the end, and the iter will return a None (Options None)
+}
